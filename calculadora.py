b64961da33aae4558e1408b3abd3ee70fd0f8a00
Stefano De Vita <devitastefano@hotmail.com>
2025-02-13 03:22:24 -0300

    aniado tests de calidad

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,32 @@
 class Calculadora:
-    def __init__(self):
-        pass
-
+    """
+    Clase que representa una calculadora básica con operaciones aritméticas.
+    """
     def suma(self, a, b):
+        """
+        Realiza la suma de dos números.
+        :param a: Primer número.
+        :param b: Segundo número.
+        :return: Resultado de la suma.
+        """
         return a + b
 
     def resta(self, a, b):
+        """
+        Realiza la resta de dos números.
+        :param a: Primer número.
+        :param b: Segundo número.
+        :return: Resultado de la resta.
+        """
         return a - b
 
     def multiplicacion(self, a, b):
+        """
+        Realiza la multiplicación de dos números.
+        :param a: Primer número.
+        :param b: Segundo número.
+        :return: Resultado de la multiplicación.
+        """
         return a * b
 
     def division(self, a, b):
